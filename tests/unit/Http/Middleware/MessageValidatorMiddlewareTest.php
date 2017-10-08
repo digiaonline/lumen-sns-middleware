e1b96d8f589dab438ba7801d59f8075cc06f0bea
Sam Stenvall <sam.stenvall@nordsoftware.com>
2017-10-08 13:47:28 +0300

    Add a missing test case for MessageValidatorMiddleware

--- tests/unit/Http/Middleware/MessageValidatorMiddlewareTest.php
+++ tests/unit/Http/Middleware/MessageValidatorMiddlewareTest.php
@@ -2,10 +2,12 @@
 
 namespace Digia\Lumen\SnsMiddleware\Tests\Http\Middleware;
 
+use Aws\Sns\Message;
 use Aws\Sns\MessageValidator;
 use Digia\Lumen\SnsMiddleware\Http\Middleware\MessageValidatorMiddleware;
 use Digia\Lumen\SnsMiddleware\Tests\TestCase;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 /**
  * Class MessageValidatorMiddlewareTest
@@ -30,6 +32,41 @@ class MessageValidatorMiddlewareTest extends TestCase
         $middleware->handle($request, function () {
 
         });
+    }
+
+    /**
+     * Tests that the middleware chain is continued if the validation succeeds
+     */
+    public function testValidMessage()
+    {
+        $json               = $this->getResourceAsString('valid_SubscriptionConfirmation_message.json');
+        $request            = new Request([], [], [], [], [], [], $json);
+        $validationCallback = function () {
+            return false;
+        };
+
+        $middleware = new MessageValidatorMiddleware(new DummyMessageValidator($validationCallback));
+
+        $this->assertInstanceOf(Response::class, $middleware->handle($request, function () {
+            return new Response();
+        }));
+    }
+
+}
+
+/**
+ * Class DummyMessageValidator
+ * @package Digia\Lumen\SnsMiddleware\Http\Middleware
+ */
+class DummyMessageValidator extends MessageValidator
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function validate(Message $message)
+    {
+
     }
 
 }
